40ee9d0025816b2251dba3aec88e44c30b3e1c9f
icediver <ageev75@gmail.com>
2024-07-27 12:13:25 +0300

    prepared to gh-pages

--- next.config.js
+++ next.config.js
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 
 const nextConfig = {
+  output: "export",
+  basePath: "/keyboard-trainer-frontend",
   env: {
     SERVER_URL: process.env.SERVER_URL,
     APP_URL: process.env.APP_URL,
   },
   images: {
+    unoptimized: true,
     domains: [
       "loremflickr.com",
       "www.aptronixindia.com",
